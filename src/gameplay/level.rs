a587dea151ed14d6e116e56449af81e34298888a
meepleek <ergocrab@gmail.com>
2026-05-07 21:38:45 +0200

    feat: draw tile grid

--- src/gameplay/level.rs
+++ src/gameplay/level.rs
@@ -4,7 +4,9 @@ use bevy::prelude::*;
 
 use crate::{gameplay::player::player, screens::Screen};
 
-pub(super) fn plugin(_app: &mut App) {}
+pub(super) fn plugin(app: &mut App) {
+    app.add_systems(Update, draw_grid);
+}
 
 /// A system that spawns the main level.
 pub fn spawn_level(mut commands: Commands) {
@@ -15,4 +17,14 @@ pub fn spawn_level(mut commands: Commands) {
         DespawnOnExit(Screen::Gameplay),
         children![player(),],
     ));
+}
+
+pub fn draw_grid(mut gizmos: Gizmos) {
+    let tile_size = 64.;
+    gizmos.grid_2d(
+        Isometry2d::from_translation(Vec2::splat(tile_size / 2.)),
+        UVec2::splat(32),
+        Vec2::splat(tile_size),
+        Color::BLACK,
+    );
 }
